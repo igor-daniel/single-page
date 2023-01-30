49e1af8baaa78a2816f348906278a974540a7d73
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-01-30 00:01:28 -0300

    header pronto

---
+++ src/components/header/openBox.js
@@ -0,0 +1,6 @@
+const openBox = ()=>{
+  const box = document.querySelector('.box-near')
+  box.classList.toggle('block')
+  // box.classList.toggle('block');
+}
+export default openBox
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,1 +1,31 @@
-export { }
+import React from "react";
+import './main.css'
+import ImgDesktop from '../img/image-hero-desktop.png'
+import data from '../img/client-databiz.svg'
+import audio from '../img/client-audiophile.svg'
+import meet from '../img/client-meet.svg'
+import maker from '../img/client-maker.svg'
+
+export const Main = () => {
+  return (
+    <main>
+      <div className="content-main">
+        <h1>Make remote work </h1>
+        <p>Get your team in sync, no matter your location.
+          Streamline processes, create team rituals, and
+          watch productivity soar.</p>
+        <button>Lear more</button>
+
+        <div className="block-images">
+          <img src={data} />
+          <img src={audio} />
+          <img src={meet} />
+          <img src={maker} />
+        </div>
+      </div>
+      <div className="image">
+        <img src={ImgDesktop} alt="" />
+      </div>
+    </main>
+  );
+}
