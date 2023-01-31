e9867f72cb7e0f225d2064675d389dddc8dd69f5
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-01-31 15:48:58 -0300

    The change font

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css'
 import { Header } from './components/header/Header'
 import { Main } from './components/main/main';
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@500;700&display=swap');
+</style>
 
 function App() {
   return (
